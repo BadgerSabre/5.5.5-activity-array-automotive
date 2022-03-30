35c697cbef3fb3f7698cffede4f0e761229a0d52
Brandon Dega <brandondega@gmail.com>
2022-03-30 13:45:50 -0500

    added new class

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,5 +1,6 @@
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
+import Vehicle from './vehicle.js'
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
@@ -7,11 +8,36 @@ console.log(v.make)
 
 class mercurySedan extends Vehicle {
     constructor(maxPass, pass, wheelNum, maxSpeed, fuel, scheduledService){
-        this.maxPass = maxPass;
-        this.pass = pass;
-        this.wheelNum = wheelNum;
-        this.maxSpeed = maxSpeed;
-        this.fuel = fuel;
-        this.scheduledService = scheduledService;
+        super(make, model, year, color, mileage)
+        this.maxPass = 6;
+        this.pass = 0;
+        this.wheelNum = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        this.scheduledService = false;
+    }
+
+    loadPassenger(num){
+        if(this.pass + num < this.maxPass){
+            console.log('There is enough room for passengers.')
+        } else {
+            console.log('There is no more room for passengers.')
+        }
+    }
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
+    scheduledService(mileage){
+        if(this.mileage > 30000){
+            this.scheduledService = true
+        }
     }
 }
+
+mercurySedan.availableRoom()
