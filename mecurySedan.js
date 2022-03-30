06588d579edf593ea83140d07ef71593083e1f16
Brandon Dega <brandondega@gmail.com>
2022-03-30 13:02:03 -0500

    first commit

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,14 @@ const VehicleModule = require("./vehicle")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class mercurySedan extends Vehicle {
+    constructor(maxPass, pass, wheelNum, maxSpeed, fuel, scheduledService){
+        this.maxPass = maxPass;
+        this.pass = pass;
+        this.wheelNum = wheelNum;
+        this.maxSpeed = maxSpeed;
+        this.fuel = fuel;
+        this.scheduledService = scheduledService;
+    }
+}
